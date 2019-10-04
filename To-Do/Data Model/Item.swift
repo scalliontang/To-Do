e89cfa25da391ff0f6d270922374443394f99e68
Sicong Tang <scalliontang@gmail.com>
2019-10-04 11:42:06 -0400

    Convert to MVC design pattern

---
+++ To-Do/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  To-Do
+//
+//  Created by Sicong Tang on 10/4/19.
+//  Copyright © 2019 Sicong Tang. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done: Bool = false
+}
